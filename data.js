022eb6573e10dfffc3dafe8624ebf7c6a3bea356
yulyazbrv <zubareva.yo@gmail.com>
2023-12-11 17:41:26 +0300

    feat: add mobile version

---
+++ data.js
@@ -0,0 +1,161 @@
+var projects = [
+  {
+    img: "images/spring-boot.svg",
+    name: "Spring Boot",
+    info: "Takes an opinionated view of building Spring applications and gets you up and running as quickly as possible",
+  },
+  {
+    img: "images/spring-framework.svg",
+    name: "Spring Framework",
+    info: "Provides core support for dependency injection, transaction management, web apps, data access, messaging, and more.",
+  },
+  {
+    img: "images/spring-data.svg",
+    name: "Spring Data",
+    info: "Provides a consistent approach to data access – relational, non-relational, map-reduce, and beyond.",
+  },
+  {
+    img: "images/spring-cloud.svg",
+    name: "Spring Cloud",
+    info: "Provides a set of tools for common patterns in distributed systems. Useful for building and deploying microservices.",
+  },
+  {
+    img: "images/spring-data-flow.svg",
+    name: "Spring Cloud Data",
+    info: "Provides an orchestration service for composable data microservice applications on modern runtimes.",
+  },
+  {
+    img: "images/spring-security.svg",
+    name: "Spring Security",
+    info: "Protects your application with comprehensive and extensible authentication and authorization support.",
+  },
+  {
+    img: "images/spring-graphql.svg",
+    name: "Spring for GraphQL",
+    info: "Spring for GraphQL provides support for Spring applications built on GraphQL Java.",
+  },
+  {
+    img: "images/spring-statemachine.svg",
+    name: "Spring Session",
+    info: "Provides an API and implementations for managing a user’s session information.",
+  },
+  {
+    img: "images/spring-authorization-server.svg",
+    name: "Spring Authorization Server",
+    info: "Provides a secure, light-weight, and customizable foundation for building OpenID Connect 1.0 Identity Providers and OAuth2 Authorization Server products.",
+  },
+  {
+    img: "images/spring-integration.svg",
+    name: "Spring Integration",
+    info: "Supports the well-known Enterprise Integration Patterns through lightweight messaging and declarative adapters.",
+  },
+  {
+    img: "images/spring-hateoas.svg",
+    name: "Spring HATEOAS",
+    info: "Simplifies creating REST representations that follow the HATEOAS principle.",
+  },
+  {
+    img: "images/spring-modulith.svg",
+    name: "Spring Modulith",
+    info: "Spring Modulith allows developers to build well-structured Spring Boot applications and guides developers in finding and working with application modules driven by the domain.",
+  },
+  {
+    img: "images/spring-batch.svg",
+    name: "Spring Batch",
+    info: "Simplifies and optimizes the work of processing high-volume batch operations.",
+  },
+  {
+    img: "images/spring-amqp.svg",
+    name: "Spring AMQP",
+    info: "Applies core Spring concepts to the development of AMQP-based messaging solutions.",
+  },
+  {
+    img: "images/spring.svg",
+    name: "Spring for Apache Pulsar",
+    info: "Provides Familiar Spring Abstractions for Apache Pulsar.",
+  },
+  {
+    img: "images/spring-flo.svg",
+    name: "Spring Flo",
+    info: "Provides a JavaScript library that offers a basic embeddable HTML5 visual builder for pipelines and simple graphs.",
+  },
+  {
+    img: "images/spring-kafka.svg",
+    name: "Spring for Apache Kafka",
+    info: "Provides Familiar Spring Abstractions for Apache Kafka.",
+  },
+  {
+    img: "images/spring-shell.svg",
+    name: "Spring Shell",
+    info: "Makes writing and testing RESTful applications easier with CLI-based resource discovery and interaction.",
+  },
+];
+
+var firstMenuItems = [
+  { name: "Overview" },
+  { name: "Microservices" },
+  { name: "Reactive" },
+  { name: "Event Driven" },
+  { name: "Cloud" },
+  { name: "Web Applications" },
+  { name: "Serverless" },
+  { name: "Batch" },
+];
+
+var secondMenuItems = [
+  { name: "Overview" },
+  { name: "Quickstart" },
+  { name: "Guides" },
+  { name: "Blog" },
+];
+
+var thirdMenuItems = [
+  { name: "Overview" },
+  { name: "Spring Boot" },
+  { name: "Spring Framework" },
+  { name: "Spring Cloud" },
+  { name: "Spring Cloud Data Flow" },
+  { name: "Spring Data" },
+  { name: "Spring Integration" },
+  { name: "Spring Batch" },
+  { name: "Spring Security" },
+  { name: "View all projects" },
+  { name: "" },
+  { name: "DEVELOPMENT TOOLS" },
+  { name: "Spring Tools 4" },
+  { name: "Spring Initializr" },
+];
+
+var fourthMenuItems = [{ name: "Courses" }, { name: "Get Certified" }];
+
+var fifthMenuItems = [
+  { name: "Overview" },
+  { name: "Spring Runtime" },
+  { name: "Spring Consulting" },
+  { name: "Spring Academy For Teams" },
+  { name: "Security Advisories" },
+];
+
+var sixthMenuItems = [
+  { name: "Overview" },
+  { name: "Events" },
+  { name: "Team" },
+];
+
+var menuItems = {
+  ".hidden-menu1": firstMenuItems,
+  ".hidden-menu2": secondMenuItems,
+  ".hidden-menu3": thirdMenuItems,
+  ".hidden-menu4": fourthMenuItems,
+  ".hidden-menu5": fifthMenuItems,
+  ".hidden-menu6": sixthMenuItems,
+};
+
+var mobileMenuItems = {
+  ".first-mobile-hidden-menu": firstMenuItems,
+  ".second-mobile-hidden-menu": secondMenuItems,
+  ".third-mobile-hidden-menu": thirdMenuItems,
+  ".fourth-mobile-hidden-menu": fourthMenuItems,
+  ".fifth-mobile-hidden-menu": fifthMenuItems,
+  ".sixth-mobile-hidden-menu": sixthMenuItems,
+};
--- script.js
+++ script.js
@@ -1,156 +1,3 @@
-const projects = [
-  {
-    img: "images/spring-boot.svg",
-    name: "Spring Boot",
-    info: "Takes an opinionated view of building Spring applications and gets you up and running as quickly as possible",
-  },
-  {
-    img: "images/spring-framework.svg",
-    name: "Spring Framework",
-    info: "Provides core support for dependency injection, transaction management, web apps, data access, messaging, and more.",
-  },
-  {
-    img: "images/spring-data.svg",
-    name: "Spring Data",
-    info: "Provides a consistent approach to data access – relational, non-relational, map-reduce, and beyond.",
-  },
-  {
-    img: "images/spring-cloud.svg",
-    name: "Spring Cloud",
-    info: "Provides a set of tools for common patterns in distributed systems. Useful for building and deploying microservices.",
-  },
-  {
-    img: "images/spring-data-flow.svg",
-    name: "Spring Cloud Data",
-    info: "Provides an orchestration service for composable data microservice applications on modern runtimes.",
-  },
-  {
-    img: "images/spring-security.svg",
-    name: "Spring Security",
-    info: "Protects your application with comprehensive and extensible authentication and authorization support.",
-  },
-  {
-    img: "images/spring-graphql.svg",
-    name: "Spring for GraphQL",
-    info: "Spring for GraphQL provides support for Spring applications built on GraphQL Java.",
-  },
-  {
-    img: "images/spring-statemachine.svg",
-    name: "Spring Session",
-    info: "Provides an API and implementations for managing a user’s session information.",
-  },
-  {
-    img: "images/spring-authorization-server.svg",
-    name: "Spring Authorization Server",
-    info: "Provides a secure, light-weight, and customizable foundation for building OpenID Connect 1.0 Identity Providers and OAuth2 Authorization Server products.",
-  },
-  {
-    img: "images/spring-integration.svg",
-    name: "Spring Integration",
-    info: "Supports the well-known Enterprise Integration Patterns through lightweight messaging and declarative adapters.",
-  },
-  {
-    img: "images/spring-hateoas.svg",
-    name: "Spring HATEOAS",
-    info: "Simplifies creating REST representations that follow the HATEOAS principle.",
-  },
-  {
-    img: "images/spring-modulith.svg",
-    name: "Spring Modulith",
-    info: "Spring Modulith allows developers to build well-structured Spring Boot applications and guides developers in finding and working with application modules driven by the domain.",
-  },
-  {
-    img: "images/spring-batch.svg",
-    name: "Spring Batch",
-    info: "Simplifies and optimizes the work of processing high-volume batch operations.",
-  },
-  {
-    img: "images/spring-amqp.svg",
-    name: "Spring AMQP",
-    info: "Applies core Spring concepts to the development of AMQP-based messaging solutions.",
-  },
-  {
-    img: "images/spring.svg",
-    name: "Spring for Apache Pulsar",
-    info: "Provides Familiar Spring Abstractions for Apache Pulsar.",
-  },
-  {
-    img: "images/spring-flo.svg",
-    name: "Spring Flo",
-    info: "Provides a JavaScript library that offers a basic embeddable HTML5 visual builder for pipelines and simple graphs.",
-  },
-  {
-    img: "images/spring-kafka.svg",
-    name: "Spring for Apache Kafka",
-    info: "Provides Familiar Spring Abstractions for Apache Kafka.",
-  },
-  {
-    img: "images/spring-shell.svg",
-    name: "Spring Shell",
-    info: "Makes writing and testing RESTful applications easier with CLI-based resource discovery and interaction.",
-  },
-];
-
-const firstMenuItems = [
-  { name: "Overview" },
-  { name: "Microservices" },
-  { name: "Reactive" },
-  { name: "Event Driven" },
-  { name: "Cloud" },
-  { name: "Web Applications" },
-  { name: "Serverless" },
-  { name: "Batch" },
-];
-
-const secondMenuItems = [
-  { name: "Overview" },
-  { name: "Quickstart" },
-  { name: "Guides" },
-  { name: "Blog" },
-];
-
-const thirdMenuItems = [
-  { name: "Overview" },
-  { name: "Spring Boot" },
-  { name: "Spring Framework" },
-  { name: "Spring Cloud" },
-  { name: "Spring Cloud Data Flow" },
-  { name: "Spring Data" },
-  { name: "Spring Integration" },
-  { name: "Spring Batch" },
-  { name: "Spring Security" },
-  { name: "View all projects" },
-  { name: "" },
-  { name: "DEVELOPMENT TOOLS" },
-  { name: "Spring Tools 4" },
-  { name: "Spring Initializr" },
-];
-
-const fourthMenuItems = [{ name: "Courses" }, { name: "Get Certified" }];
-
-const fifthMenuItems = [
-  { name: "Overview" },
-  { name: "Spring Runtime" },
-  { name: "Spring Consulting" },
-  { name: "Spring Academy For Teams" },
-  { name: "Security Advisories" },
-];
-
-const sixthMenuItems = [
-  { name: "Overview" },
-  { name: "Events" },
-  { name: "Team" },
-];
-
-const menuItems = {
-  ".hidden-menu1": firstMenuItems,
-  ".hidden-menu2": secondMenuItems,
-  ".hidden-menu3": thirdMenuItems,
-  ".hidden-menu4": fourthMenuItems,
-  ".hidden-menu5": fifthMenuItems,
-  ".hidden-menu6": sixthMenuItems,
-};
-
 function setProjects() {
   const container = document.getElementById("cont-prj");
 
@@ -190,4 +37,37 @@ function getMenuItemsHTML(classname) {
     .map((item) => `<li>${item.name}</li>`)
     .join("");
   return `<ul>${itemsHTML}</ul>`;
+}
+
+function showMobileMenu() {
+  const menu = document.querySelector(".mobile-menu");
+  menu.style.display = "block";
+}
+
+function closeMobileMenu() {
+  const menu = document.querySelector(".mobile-menu");
+  menu.style.display = "none";
+}
+
+let click = 0;
+function showMobileMenuItems(classname) {
+  const menu = document.querySelector(classname);
+  console.log(menu);
+  const items = mobileMenuItems[classname];
+
+  if (click % 2 !== 0) {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+    menu.innerHTML = items
+      .map(
+        (item) => `
+          <div>
+            <h2 class="menu-title">${item.name}</h2>
+          </div>`
+      )
+      .join("");
+  }
+
+  click++;
 }
